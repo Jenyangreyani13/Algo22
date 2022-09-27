a7b694f67cf04aa3a9ee38c9520e0cc0876aa090
Jenyangreyani13 <114489140+Jenyangreyani13@users.noreply.github.com>
2022-09-27 20:10:00 +0700

    Add files via upload

---
+++ 27 september.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int pil; 
+	cout<<"pil: ";cin>>pil;
+	switch (pil){
+		case 1 : cout<<"algoritma"<<endl;
+				 cout<<"dan"<<endl;
+				 cout<<"pemprograman"<<endl;
+				 break;
+		case 2 : cout<<"matematika"<<endl;
+				 cout<<"diskrit"<<endl;
+				 break;
+		default : cout<<"bahasa"<<endl;
+		cout<<"inggris"<<endl;
+	}
+}
